1455c85a1cee7b1e9c86d0d6fdc1c49f5baba879
Tim B <tbatty7@gmail.com>
2020-02-03 20:18:47 -0500

    added bad request test to integration tests

--- src/test/java/com/battybuilds/testexamples/integration/RestTemplateIntegrationTest.java
+++ src/test/java/com/battybuilds/testexamples/integration/RestTemplateIntegrationTest.java
@@ -8,13 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.Matchers.any;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -26,13 +27,19 @@ public class RestTemplateIntegrationTest {
     RestTemplate restTemplate;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         mockRestServiceServer = MockRestServiceServer.createServer(restTemplate);
     }
 
     @Test(expected = HttpServerErrorException.InternalServerError.class)
     public void throwsExceptionOn500() {
         mockRestServiceServer.expect(requestTo(any(String.class))).andRespond(withServerError());
+        restTemplate.exchange("http://url.com", HttpMethod.GET, null, String.class);
+    }
+
+    @Test(expected = HttpClientErrorException.BadRequest.class)
+    public void throwsExceptionOn400() {
+        mockRestServiceServer.expect(requestTo(any(String.class))).andRespond(withBadRequest());
         restTemplate.exchange("http://url.com", HttpMethod.GET, null, String.class);
     }
 }
